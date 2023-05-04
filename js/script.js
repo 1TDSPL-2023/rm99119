11e1d2291ccf2b6895432aa36cae683274be414b
LeoBlanco123 <rm99119@fiap.com.br>
2023-05-04 09:48:05 -0300

    convertendo htmlcoletion em array

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-//Atrelando o evento click ao elemento h2
-// const h2Element = document.getElementById("titulo");
-
-// h2Element.addEventListener("click", ()=>{
-//     //Math função matemática...
-//     //Os metódos floor, round e ceil eles arredondam os números passados.
 //     //O método randon retorna um número entre 0 e 1. Ex: 0.1233.
 //     let r = "";
 //     let g = "";
@@ -87,3 +81,28 @@
 //     console.log(fruta);
 // } );
 
+// let nr1 = [1,2,3,4,5];
+// let nr2 = [6,7,8,9,10];
+
+// //Ao invés de concatenar os arrays vamos utilizar o operador SPREAD( ... ) para melhorar a inserç~Qao dos dados no novo array.
+// //CONCATENAÇÃO DE ARRAYS:
+// // let nr3 = [nr1,nr2];
+// let nr3 = [...nr1,...nr2];
+
+// //Imprimindo o novo array concatenado.
+// console.log("NOVO ARRAY : " + nr3);
+// //Aplicando o forEach no novo array
+// nr3.forEach((nr)=>{
+//     console.log("ITENS DO NOVO ARRAY : " + nr);
+// });
+
+//Converter HTMLElements em Array
+const aElements = document.getElementsByTagName("a");
+console.log(aElements);
+const aElementsArray = [...aElements];
+console.log(aElementsArray);
+
+//Imprimindo atributos dos elementos recuperados...
+aElementsArray.forEach((a)=>{
+    console.log(a.href);
+});
